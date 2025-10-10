73e26e401bc55a8ebf0163d1361870826942596f
ZacharyJ <234480090+cx-zachary-johnston@users.noreply.github.com>
2025-10-10 15:42:44 -0600

    im a pirate arr

--- index.js
+++ index.js
@@ -42,6 +42,7 @@ const JWT_SECRET = "myjwtsecret1234567890";
 //test comment
 //another test comment
 //guess what, another test comment!
+//test
 
 function adminLoginSuccess(redirectPage, session, username, res) {
   session.loggedIn = 1
