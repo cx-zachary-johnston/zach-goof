97f8744c0a279bd58a113b9da094e37477a5490b
ZacharyJ <234480090+cx-zachary-johnston@users.noreply.github.com>
2025-10-21 17:40:49 -0600

    added OG index.js content from other GH account

--- index.js
+++ index.js
@@ -39,9 +39,39 @@ const DATABASE_PASSWORD = "SuperSecretP@ssw0rd!";
 const AWS_SECRET_ACCESS_KEY = "AKIAIOSFODNN7EXAMPLExwJ8fM0qFDSH";
 const JWT_SECRET = "myjwtsecret1234567890";
 
-//test comment
-//another test comment
-//guess what, another test comment!
+exports.loginHandler = function (req, res, next) {
+  if (validator.isEmail(req.body.username)) {
+    User.find({ username: req.body.username, password: req.body.password }, function (err, users) {
+      if (users.length > 0) {
+        const redirectPage = req.body.redirectPage
+        const session = req.session
+        const username = req.body.username
+        return adminLoginSuccess(redirectPage, session, username, res)
+      } else {
+        return res.status(401).send()
+      }
+    });
+  } else {
+    return res.status(401).send()
+  }
+};
+
+exports.loginHandler = function (req, res, next) {
+  if (validator.isEmail(req.body.username)) {
+    User.find({ username: req.body.username, password: req.body.password }, function (err, users) {
+      if (users.length > 0) {
+        const redirectPage = req.body.redirectPage
+        const session = req.session
+        const username = req.body.username
+        return adminLoginSuccess(redirectPage, session, username, res)
+      } else {
+        return res.status(401).send()
+      }
+    });
+  } else {
+    return res.status(401).send()
+  }
+};
 
 function adminLoginSuccess(redirectPage, session, username, res) {
   session.loggedIn = 1
